a9d45561ed7a8d35de1a3227633005cdb4ee0a7e
sehyunyuk <sdfg8931@ajou.ac.kr>
2025-05-21 00:58:45 +0900

    FEAT: ADD search button

--- crawl-ts/src/server/routes/jobRouter.ts
+++ crawl-ts/src/server/routes/jobRouter.ts
@@ -14,11 +14,11 @@ router.get('/job', async (req: Request ,res: Response): Promise<any> => {
           model: MysqlJobValidTypeSequelize,
           as: 'jobValidTypes',
           attributes: ['valid_type'],
-          where: {
-            valid_type: {
-              [Op.is]: null
-            }
-          },
+          // where: {
+          //   valid_type: {
+          //     [Op.is]: null
+          //   }
+          // },
           required: false
         }
       ]
@@ -27,6 +27,34 @@ router.get('/job', async (req: Request ,res: Response): Promise<any> => {
     if (!recruitInfo || recruitInfo.length === 0) {
       return res.status(404).json({ error: 'Job not found' });
     }
+    return res.json({ data: recruitInfo });
+  } catch (error) {
+    console.error('Error fetching job:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+// 라우터 정의
+router.post('/job-valid-type', async (req: Request ,res: Response): Promise<any> => {
+  try {
+    if (!req.query.job_id) {
+      return res.status(400).json({ error: 'job_id is required' });
+    }
+    if(!req.query.valid_type) {
+      return res.status(400).json({ error: 'valid_type is required' });
+    }
+    const job_id = parseInt(req.query.job_id as string, 10);
+    const valid_type = parseInt(req.query.valid_type as string, 10);
+    const recruitInfo = await MysqlJobValidTypeSequelize.upsert({
+      job_id,
+      valid_type
+    });
+
+    if(!recruitInfo) {
+      return res.status(404).json({ error: 'Job not found' });
+    }
+
     return res.json({ data: recruitInfo });
   } catch (error) {
     console.error('Error fetching job:', error);
